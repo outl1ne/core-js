757b39ef921bdcccc33009556a5bc2254a1c4d39
Matis Lepik <matis.lepik@gmail.com>
2016-11-11 14:25:38 +0200

    Breaking: Optimized events/deviceinfo changes. Add getViewportHeight function.
    Make optimizedEvents have to be initialized with init() function.
    Expose throttle globally.
    optimizedResize now always gets called for orientation change.
    optimizedResize optionally passes new viewport information

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import './lib/optimizedEvents'; // Allows you to listen to resize/scroll/etc in a throttled way
 import './lib/polyfill/requestAnimationFrame'; // Polyfills requestAnimationFrame (CustomEvent depends on this)
 import './lib/polyfill/CustomEvent'; // Polyfills custom events
 
@@ -7,6 +6,7 @@ import * as modals from './lib/modals';
 import * as floatLabel from './lib/floatLabel';
 import * as dropdowns from './lib/dropdowns';
 import * as deviceInfo from './lib/deviceInfo';
+import * as optimizedEvents from './lib/optimizedEvents';
 
-export { initSmoothScrollToAnchor, modals, floatLabel, dropdowns, deviceInfo };
-export default { initSmoothScrollToAnchor, modals, floatLabel, dropdowns, deviceInfo };
+export { initSmoothScrollToAnchor, modals, floatLabel, dropdowns, deviceInfo, optimizedEvents };
+export default { initSmoothScrollToAnchor, modals, floatLabel, dropdowns, deviceInfo, optimizedEvents };
--- src/lib/optimizedEvents.js
+++ src/lib/optimizedEvents.js
@@ -1,29 +1,49 @@
 /**
  * OPTIMIZED EVENTS:
  * Creates events called "optimizedResize" and "optimizedScroll" that will listen to scroll/resize events,
- * optimized with requestAnimationFrame to prevent them from getting called too much. This should help
- * with performance, but will get called less often than the native scroll/resize events.
+ * optimized with requestAnimationFrame to prevent them from getting called too much. This helps with
+ * performance, but will get called less often than the native scroll/resize events.
  *
  * @module lib/optimizedEvents
  */
 
-/* eslint-disable */
+export function init(opts) {
+  /* Add throttled events */
+  throttle('resize', 'optimizedResize', opts.reportViewportSize === true && getViewportDetail);
+  throttle('scroll', 'optimizedScroll');
 
-(function() {
-    'use strict';
-    var throttle = function(type, name, obj) {
-        obj = obj || window;
-        var running = false;
-        var func = function() {
-            if (running) { return; }
-            running = true;
-            window.requestAnimationFrame(function() {
-                obj.dispatchEvent(new CustomEvent(name));
-                running = false;
-            });
-        };
-        obj.addEventListener(type, func);
-    };
-    throttle ('resize', 'optimizedResize');
-    throttle ('scroll', 'optimizedScroll');
-})();
+  /* Dispatch optimizedResize on orientation change */
+  window.addEventListener('orientationchange', () => {
+    window.dispatchEvent(
+      new CustomEvent('optimizedResize', { detail: opts.reportViewportSize === true && getViewportDetail() })
+    );
+  });
+}
+
+/**
+ * Creates a throttled version of an event
+ * @param  {string} originalEvent          Original event to throttle
+ * @param  {string} newEvent          Name of the new custom event
+ * @param  {?function} getDetail  Optional function that should return an object which will be set as the `event.detail` value. This gets called whenever the custom event is dispatched.
+ */
+export function throttle(originalEvent, newEvent, getDetail) {
+  let running = false;
+
+  window.addEventListener(originalEvent, () => {
+    if (running) { return; }
+
+    running = true;
+
+    window.requestAnimationFrame(() => {
+      window.dispatchEvent(new CustomEvent(newEvent, { detail: getDetail && getDetail() }));
+      running = false;
+    });
+  });
+}
+
+function getViewportDetail() {
+  return {
+    width: $(window).width(),
+    height: $(window).height()
+  }
+}
