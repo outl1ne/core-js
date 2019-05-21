e62b10474b01d20b2dffa2d6fc81019b3f7ee3d0
Matis Lepik <matis.lepik@gmail.com>
2019-05-21 10:54:55 +0300

    Delete old code and add microbundle

--- readme.md
+++ readme.md
@@ -1,14 +1,9 @@
-This is a collection of JS functionalities that we commonly need to use. Documentation for each module can be found in the docs folder, or source code.
+This is a collection of JS functionalities that we commonly need to use.
 
 # Docs
 
-- [Modals](https://github.com/optimistdigital/core-js/blob/master/docs/modals.md)
-- [Device info](https://github.com/optimistdigital/core-js/blob/master/docs/deviceInfo.md)
-- [Optimized events](https://github.com/optimistdigital/core-js/blob/master/docs/optimizedEvents.md)
-- [Custom events](https://github.com/optimistdigital/core-js/blob/master/docs/customEvents.md)
-- [Smooth scroll](https://github.com/optimistdigital/core-js/blob/master/docs/smoothScroll.md)
+Todo :)
 
 # Development
 
-- Build: `npm run build`
-- Testing with Mocha + jsdom: `npm test`
+-   Build: `npm run build`
--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-import './lib/polyfill/requestAnimationFrame'; // Polyfills requestAnimationFrame (CustomEvent depends on this)
-import './lib/polyfill/CustomEvent'; // Polyfills custom events
-import './lib/polyfill/arrayFind'; // Polyfills Array.prototype.find
+import modals from './modals';
+import dropdowns from './dropdowns';
 
-import { initSmoothScrollToAnchor } from './lib/smoothScroll';
-import * as modals from './lib/modals';
-import * as floatLabel from './lib/floatLabel';
-import * as dropdowns from './lib/dropdowns';
-import * as deviceInfo from './lib/deviceInfo';
-import * as optimizedEvents from './lib/optimizedEvents';
-import * as customEvents from './lib/customEvents';
-
-export { initSmoothScrollToAnchor, modals, floatLabel, dropdowns, deviceInfo, optimizedEvents, customEvents };
-export default { initSmoothScrollToAnchor, modals, floatLabel, dropdowns, deviceInfo, optimizedEvents, customEvents };
+export { modals, dropdowns };
