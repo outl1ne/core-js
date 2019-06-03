7592f2d075e959cd3c2f4f81f14591e0487eb123
Marika Must <marika@optimistdigital.com>
2019-06-03 16:13:01 +0300

    Delete examples

--- examples/client/js/entry.js
+++
@@ -1,9 +0,0 @@
-// SCSS entry. In production, the contents are extracted into a separate file. In dev, JS loads the CSS dynamically.
-import '../scss/entry.scss';
-// Polyfills
-import 'core-js/stable';
-
-import { modals, dropdowns } from '../../../dist';
-
-modals.init();
-dropdowns.init();
